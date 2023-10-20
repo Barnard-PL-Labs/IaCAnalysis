86439eabd908f005413345b223ef51abeabdcb20
Leo Qiao <qiaofeitong@hotmail.com>
2023-10-20 12:45:34 -0400

    new implementation with support for additive monthly_requests topology

--- iac_analysis/cli.py
+++ iac_analysis/cli.py
@@ -5,9 +5,12 @@ from typing import Optional
 import typer
 from typing_extensions import Annotated
 import logging
+import z3
 
 from iac_analysis import __app_name__, __version__
 import iac_analysis.analysis as analysis
+from iac_analysis.infrastructure import Infrastructure
+from iac_analysis import infracost
 
 
 app = typer.Typer()
@@ -49,6 +52,26 @@ def main(
     logging.basicConfig(level=logging_level)
 
 
+# @app.command()
+# def check(
+#     tf_plan: Annotated[str, typer.Argument(help="Terraform plan file in JSON")],
+#     ic_usage: Annotated[str, typer.Argument(help="Infracost usage file in YAML")],
+# ) -> None:
+#     """
+#     Check whether the Infracost usage estimates satisfy the constraints of the Terraform infrastructures.
+#     """
+#     result = analysis.run_iac_analysis_with_paths(tf_plan, ic_usage)
+
+#     if result == analysis.SAT:
+#         print("✅ The usage estimates satisfy the constraints of the infrastructure")
+#     elif result == analysis.UNSAT:
+#         print(
+#             "❌ The usage estimates did not satisfy the constraints of the infrastructure"
+#         )
+#     else:
+#         print("⚠️ The solver failed to solve the constraints")
+
+
 @app.command()
 def check(
     tf_plan: Annotated[str, typer.Argument(help="Terraform plan file in JSON")],
@@ -57,11 +80,25 @@ def check(
     """
     Check whether the Infracost usage estimates satisfy the constraints of the Terraform infrastructures.
     """
-    result = analysis.run_iac_analysis_with_paths(tf_plan, ic_usage)
+    infra = Infrastructure.from_terraform_plan_path(tf_plan)
+    solver = z3.Solver()
 
-    if result == analysis.SAT:
+    # infrastructure constraints
+    for constraint in infra.constraints:
+        solver.add(constraint)
+
+    # infracost usage constraints
+    usage = infracost.read_usage_yaml(ic_usage)
+    usage_constraints = infra.generate_infracost_usage_constraints(usage)
+    for constraint in usage_constraints:
+        solver.add(constraint)
+
+    result = solver.check()
+    print("z3 solver constraints: \n%s", solver.sexpr())
+
+    if result == z3.sat:
         print("✅ The usage estimates satisfy the constraints of the infrastructure")
-    elif result == analysis.UNSAT:
+    elif result == z3.unsat:
         print(
             "❌ The usage estimates did not satisfy the constraints of the infrastructure"
         )

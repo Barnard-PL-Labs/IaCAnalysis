344fc10c5609159abdf05b590a1b6bdb39443a07
Leo Qiao <qiaofeitong@hotmail.com>
2023-12-06 12:55:35 -0500

    fixes

--- examples/cdk/typescript/s3-sns-lambda-chain/lambda/lambda.d.ts
+++
@@ -1,5 +0,0 @@
-export interface LambdaResponse {
-    statusCode: number;
-    body: any;
-}
-export declare const handler: (event: Record<string, any>) => Promise<LambdaResponse>;
--- examples/cdk/typescript/s3-sns-lambda-chain/lib/s3-sns-sqs-lambda-chain-stack.d.ts
+++
@@ -1,4 +0,0 @@
-import { App, Stack, StackProps } from 'aws-cdk-lib';
-export default class S3SnsSqsLambdaChainStack extends Stack {
-    constructor(scope: App, id: string, props?: StackProps);
-}

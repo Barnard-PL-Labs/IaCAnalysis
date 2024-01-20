840ccbb709f09dd16ecd9142c4353ef01f59c44a
Leo Qiao <qiaofeitong@hotmail.com>
2024-01-20 15:43:30 -0500

    generate the constraints that variables >= 0

--- iac_analysis/infra.py
+++ iac_analysis/infra.py
@@ -4,6 +4,7 @@ from iac_analysis.resource import (
     supported_resource_types,
     ignore_resource_types,
     public_usage_metrics,
+    metrics,
 )
 import logging
 import networkx as nx
@@ -24,8 +25,9 @@ class Infra:
             self.graph.add_node(r)
             for inn in r.incoming_edges:
                 self.graph.add_edge(inn, r)
-        if not nx.is_directed_acyclic_graph(self.graph):
-            logger.error("Graph is cyclic.")
+        # INFO: we can actually allow cyclic graphs and they can be useful
+        # if not nx.is_directed_acyclic_graph(self.graph):
+        #     logger.error("Graph is cyclic.")
 
     @classmethod
     def from_cfn(cls, fpath):
@@ -50,9 +52,18 @@ class Infra:
                 print(f"outgoing <-- {outgoing_r.name}")
 
     def compute_constraints(self, solver, custom_generator=None):
+        # instantiate the solver variables and basic constraints
+        for _, r in self.resources.items():
+            for metric in metrics[r.resource_type]:
+                solver.add(solver.nv(r, metric) >= 0)
+                # TODO: this is true given the current supported metrics but might not be true for all edge variables
+                # At that point, should probably put this in the resource's compute_constraints function
+                for inn in r.incoming_edges:
+                    solver.add(solver.ev(inn, r, metric) >= 0)
+
         # compute constraints in topological order
         # topological order is needed for computing the incoming constraints
-        for r in nx.topological_sort(self.graph):
+        for _, r in self.resources.items():
             r.compute_constraints(
                 solver, self.resources, custom_generator=custom_generator
             )
